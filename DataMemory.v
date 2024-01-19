a77b376ca0791d35ec39b44292ace5e68db0cad6
ArbanaNeziraj <116509042+ArbanaNeziraj@users.noreply.github.com>
2024-01-19 22:37:05 +0100

    DataMemory.v

--- DataMemory.v
+++ DataMemory.v
@@ -3,11 +3,7 @@
 // Company: 
 // Engineer: 
 // 
-<<<<<<< HEAD
-// Create Date: 01/19/2024 09:50:58 PM
-=======
 // Create Date: 19.01.2024 15:24:58
->>>>>>> eca4e703d62e093eb2223c5e00eab103893249cc
 // Design Name: 
 // Module Name: DataMemory
 // Project Name: 
@@ -25,20 +21,6 @@
 
 
 module DataMemory(
-<<<<<<< HEAD
-    input wire[15:0] Address,
-    input wire[15:0] WriteData,
-    input wire MemWrite,
-    input wire MemRead,
-    input wire Clock,
-    output wire [15:0] ReadData
-    );
-    
-    reg[7:0] dataMem[127:0];
-
-initial
-$readmemb("dataMemory.mem", dataMem);
-=======
 input wire[15:0] Address,
 input wire[15:0] WriteData,
 input wire MemWrite,
@@ -49,40 +31,24 @@ output wire[15:0] ReadData
     reg[7:0] dMem[127:0];
 
 initial $readmemh("dataMemory.mem", dMem);
->>>>>>> eca4e703d62e093eb2223c5e00eab103893249cc
 
 always@(posedge Clock)
 begin
     if(MemWrite) 
         begin
             //bigEndian
-<<<<<<< HEAD
-            dataMem[Address + 16'd0] <= WriteData[15:8];
-            dataMem[Address + 16'd1] <= WriteData[7:0];
-=======
             dMem[Address + 16'd0] <= WriteData[15:8];
             dMem[Address + 16'd1] <= WriteData[7:0];
->>>>>>> eca4e703d62e093eb2223c5e00eab103893249cc
            end
 end
 
 always@(negedge Clock)
 begin
-<<<<<<< HEAD
-$writememb("dataMemory.mem", dataMem);
-end
-
- 
-assign ReadData[15:8] = dataMem[Address + 16'd0];
-assign ReadData[7:0] = dataMem[Address + 16'd1];
-
-=======
 $writememb("dataMemory.mem", dMem);
 end
 
  
  assign ReadData[15:8] = dMem[Address + 16'd0];
  assign ReadData[7:0] = dMem[Address + 16'd1];
->>>>>>> eca4e703d62e093eb2223c5e00eab103893249cc
 
 endmodule
